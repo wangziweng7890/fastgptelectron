853c6af81f0c4960358cf65e0953e511a3f8cd24
Kennen梁智开 <kennen.lzk@galaxyoversea.com>
2024-03-15 10:10:41 +0800

    feat: remove login

--- src/services/axios-interceptors.ts
+++ src/services/axios-interceptors.ts
@@ -7,9 +7,8 @@ import type { Middleware, ResponseError } from './types'
 const router = useRouter()
 function login() {
   console.log('跳转登录')
-  router.push({
-    path: '/login',
-  })
+  // TODO 企业微信重新授权
+  // location.href = '/'
 }
 
 // 如果不想通过拦截器统一弹出axios的error message, 在axios的config中加入key是 HideAxiosErrorToastKey ，value：true的选项
