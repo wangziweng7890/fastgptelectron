786a644c9141e2bb094e86c76117a229520c1310
Steve钟毅 <steve.zy@galaxyoversea.com>
2024-03-21 10:22:36 +0800

    fix: apifox.config.js projectId

--- apifox.config.js
+++ apifox.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 选择你的项目id反注释掉就行，只支持一个projectId
-  projectId: 2581365, // 主流程组-MVP工作台
+  projectId: 4130721, // smart
   // projectId: 2581366, // C端项目组-全IT
   // 如果Authorization过期，直接访问https://app.apifox.com/main从某个接口的请求头里面copy Authorization header放到这里就行
   Authorization:
